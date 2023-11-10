840f5bde57903037b7f11079f3c0bdc6c9656825
saikat samanta <saikatsamanta052@gmail.com>
2023-11-10 17:56:32 +0530

    product card

--- src/Page/Home.jsx
+++ src/Page/Home.jsx
@@ -1,7 +1,22 @@
+import { data } from "../data"
 export default function Home() {
   return (
     <div>
-        <h1>Welcome to best fasion store online</h1>
+        <h1 className="text-4xl text-center mt-[30px]">Welcome to best fasion store online 🔥</h1>
+        <h1 className="font-semibold text-2xl">Popular products </h1>
+        <div className="card_container w-screen flex flex-wrap gap-6 justify-center mt-10">
+                {
+                        data.shoes.map(v=>{
+                                return <div className="card h-[400px] w-[300px] bg-cyan-100 rounded-md border-2">
+                                <img src={v.image} alt="" />
+                                <h2> {v.name} </h2>
+                                </div>
+                        })
+                }
+                
+        </div>
     </div>
   )
 }
+
+// Tailwind to our React project
---
+++ src/data.js
@@ -0,0 +1,75 @@
+export const data={
+        "shoes": [
+          {
+            "id": "001",
+            "name": "Running Shoes A",
+            "image": "https://pngimg.com/d/running_shoes_PNG5816.png",
+            "price": 79.99,
+            "rating": 4.5
+          },
+          {
+            "id": "002",
+            "name": "Casual Sneakers B",
+            "image": "[Image URL]",
+            "price": 49.99,
+            "rating": 4.2
+          },
+          {
+            "id": "003",
+            "name": "High-Top Boots C",
+            "image": "[Image URL]",
+            "price": 89.99,
+            "rating": 4.7
+          },
+          {
+            "id": "004",
+            "name": "Sandals D",
+            "image": "[Image URL]",
+            "price": 39.99,
+            "rating": 3.9
+          },
+          {
+            "id": "005",
+            "name": "Athletic Shoes E",
+            "image": "[Image URL]",
+            "price": 69.99,
+            "rating": 4.0
+          },
+          {
+            "id": "006",
+            "name": "Fashionable Loafers F",
+            "image": "[Image URL]",
+            "price": 59.99,
+            "rating": 4.8
+          },
+          {
+            "id": "007",
+            "name": "Hiking Boots G",
+            "image": "[Image URL]",
+            "price": 99.99,
+            "rating": 4.6
+          },
+          {
+            "id": "008",
+            "name": "Formal Shoes H",
+            "image": "[Image URL]",
+            "price": 89.99,
+            "rating": 4.3
+          },
+          {
+            "id": "009",
+            "name": "Flip Flops I",
+            "image": "[Image URL]",
+            "price": 19.99,
+            "rating": 3.5
+          },
+          {
+            "id": "010",
+            "name": "Training Sneakers J",
+            "image": "[Image URL]",
+            "price": 74.99,
+            "rating": 4.1
+          }
+        ]
+      }
+      
